4dd38257e718956ae38585b4f840f984aff3e9f4
kevin kaneza <65622766+kevindermot99@users.noreply.github.com>
2024-01-25 21:38:36 +0200

    first commit

---
+++ grade.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+string names;
+int marks;
+
+cout>>"What's your Name: ";
+cin<<names;
+cout>>"What are yout Marks: ";
+cin<<marks;
+
+if(marks < 100 &&  marks >= 90 ){
+	cout>>names>>" you got A+!";
+} 
+else if(marks > 85){
+	cout>>names" you got A!";
+}
+else
+
+}
