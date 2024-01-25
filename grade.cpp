616334cd090ee8e8baeb82414a0e2b52172e7673
kevin kaneza <65622766+kevindermot99@users.noreply.github.com>
2024-01-25 22:07:37 +0200

    Grade app complete

--- grade.cpp
+++ grade.cpp
@@ -7,47 +7,47 @@ int marks;
 
 cout<< "What's your Name: ";
 cin>>names;
-cout<< "What are yout Marks: ";
+cout<<names<<" What are your Marks: ";
 cin>>marks;
 
 if(marks < 100 &&  marks >= 90 ){
-	cout<<names<<" you got A+!";
+	cout<<names<<" you got an A+"<<endl;
 } 
 else if(marks >= 85){
-	cout<<names" you got A!";
+	cout<<names<<" you got an A"<<endl;
 }
 else if(marks >= 80){
-	cout<<names" you got A-";
+	cout<<names<<" you got an A"<<endl;
 }
 else if(marks >= 75){
-	cout<<names" you got B+";
+	cout<<names<<" you got a B+"<<endl;
 }
 else if(marks >= 70){
-	cout<<names" you got B";
+	cout<<names<<" you got a B"<<endl;
 }
 else  if(marks >= 65){
-	cout<<names" you got C+";
+	cout<<names<<" you got a C+"<<endl;
 }
 else if(marks >= 60){
-	cout<<names>>" you got C";
+	cout<<names<<" you got a C"<<endl;
 }
 else if(marks >= 55){
-	cout<<names>>" you got D+";
+	cout<<names<<" you got a D+"<<endl;
 }
-else if(marks >/= 50){
-	cout<<names>>" you got D";
+else if(marks >= 50){
+	cout<<names<<" you got a D"<<endl;
 }
 else if(marks >= 40){
-	cout<<names>>" you got E";
+	cout<<names<<" you got an E"<<endl;
 }
 else if(marks > 40){
-	cout<<names>>" you got F";
+	cout<<names<<" you got an F"<<endl;
 }
 else if(marks > 100){
-	cout<<names" you entered  Marks above 100";
+	cout<<names<<" you entered Marks above 100";
 }
 else{
-	cout<<names>>" Request Failed ! try again";
+	cout<<names<<" Request Failed ! try again";
 }
 
 return 0;
