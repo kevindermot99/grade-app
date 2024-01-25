849ee0014c0fc97da35fda9d79de353f031d1b64
kevin kaneza <65622766+kevindermot99@users.noreply.github.com>
2024-01-25 21:48:56 +0200

    if statements  + grades complete

--- grade.cpp
+++ grade.cpp
@@ -13,9 +13,34 @@ cin<<marks;
 if(marks < 100 &&  marks >= 90 ){
 	cout>>names>>" you got A+!";
 } 
-else if(marks > 85){
+else if(marks >= 85){
 	cout>>names" you got A!";
 }
-else
-
+else if(marks >= 80){
+	cout>>names" you got A-";
+}
+else if(marks >= 75){
+	cout>>names" you got B+";
+}
+else if(marks >= 70){
+	cout>>names" you got B";
+}
+else  if(marks >= 65){
+	cout>>names" you got C+";
+}
+else if(marks >= 60){
+	cout>>" you got C";
+}
+else if(marks >= 55){
+	cout>>" you got D+";
+}
+else if(marks >= 50){
+	cout>>" you got D";
+}
+else if(marks >= 40){
+	cout>>names>>" you got E";
+}
+else if(marks > 40){
+	cout>>names>>" you got F";
+}
 }
